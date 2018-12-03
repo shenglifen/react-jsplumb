cba260bd152d7e9137463dd05384263ad41f4d2c
shenglifen <lifensheng1011@gmail.com>
2018-12-03 22:24:48 +0800

    create react-app antd-demo-ts --scripts-version=react-scripts-ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,5 @@
+import 'antd/dist/antd.css';
+
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './App';
@@ -5,7 +7,7 @@ import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root') as HTMLElement
+    <App/>,
+    document.getElementById('root') as HTMLElement
 );
 registerServiceWorker();
